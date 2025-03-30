c26ce87ea42dd3ace57df428bc591d4238463201
hoshinome <hoshinome@outlook.jp>
2025-03-31 08:52:11 +0900

    v2.1.0
    KumoGen-v2.1.0

---
+++ KumoGen/addon/operator/addcloud.py
@@ -0,0 +1,36 @@
+import bpy, os
+from bpy.types import Operator
+from ..function import deleate_cloud, property_exists
+
+class CloudAddOperator(Operator):
+    bl_idname = "kumogen.addcloud"
+    bl_label = "Cloud"
+
+    @classmethod
+    def poll(cls, context):
+        if bpy.app.version >= (3, 0, 0) and True:
+            cls.poll_message_set('')
+        return not False
+
+    def execute(self, context):
+        blend_file= os.path.join(os.path.dirname(__file__), '..', 'assets', 'Cloud.blend')
+        if (property_exists.property_exists("bpy.data.collections", globals(), locals()) and ('KumoGen-Cube' in bpy.data.collections or 'KumoGen-Sphere' in bpy.data.collections)):
+            pass
+        else:
+            if not os.path.exists(blend_file):
+                self.report({'ERROR'}, f"File not found: {blend_file}")
+                return {'CANCELLED'}
+            deleate_cloud.deleate_cloud()
+            before_data = context.view_layer.active_layer_collection
+            context.view_layer.active_layer_collection = context.view_layer.layer_collection
+            wm = bpy.context.window_manager.KumoGen_Mesh_Types
+            if wm.mesh_types == 'CUBE':
+                bpy.ops.wm.append(directory=os.path.join(blend_file, 'Collection'),filename='KumoGen-Cube')
+                bpy.data.materials["KumoGen-Clouds"].node_tree.nodes["KumoGen-Scale"].inputs[0].default_value = 1
+            elif wm.mesh_types == 'SPHERE':
+                bpy.ops.wm.append(directory=os.path.join(blend_file, 'Collection'),filename='KumoGen-Sphere')
+                bpy.data.materials["KumoGen-Clouds"].node_tree.nodes["KumoGen-Scale"].inputs[0].default_value = 0
+            context.view_layer.active_layer_collection = before_data
+            bpy.ops.object.select_all(action='DESELECT')
+        
+        return {"FINISHED"}
