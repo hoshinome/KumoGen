3fd18afe796d5887c2543fe3ba7858ab93d2abe6
hoshinome <hoshinome@outlook.jp>
2025-03-31 09:11:10 +0900

    Delete KumoGen directory

--- KumoGen/addon/updater/__init__.py
+++
@@ -1,9 +0,0 @@
-import bpy
-from . import addon_updater_ops
-from ... import VERSION
-
-def register():
-	addon_updater_ops.register({"version": VERSION})
-
-def unregister():
-	addon_updater_ops.unregister()
