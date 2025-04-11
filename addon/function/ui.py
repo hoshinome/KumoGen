153355c5bb5cf2e03c209f4e9d9e8bb3eed0b9b1
hoshinome <hoshinome@outlook.jp>
2025-04-12 02:47:54 +0900

    Delete addon directory

--- addon/function/ui.py
+++
@@ -1,8 +0,0 @@
-import bpy
-
-def kumogen_prop(layout, materials, nodes, inputs, text):
-    layout.prop(bpy.data.materials[materials].node_tree.nodes[nodes].inputs[inputs], "default_value", text=text,)
-
-def kumogen_tabs (layout, wm, name):
-    layout.alignment = "LEFT"
-    layout.prop(wm, name, icon="DOWNARROW_HLT" if getattr(wm, name) else "RIGHTARROW", emboss=False)
